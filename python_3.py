02f07e91027226c3325fe58b5dd76a33e9e15ead
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2019-08-30 13:11:53 +0200

    Update python_3.py

--- python_3.py
+++ python_3.py
@@ -1,20 +1,6 @@
-fruit = {"appel": "rood", "sinaasappel": "oranje ", "kiwi": "groen"}
-matrialen = {"hout":"vlambaar", "metaal": "roest", "papier": "scheuren" }
-
-
-
-key = input("type een fruit")
-fruit["druif"] = "paars"
-del fruit["kiwi"]
-if key == "appel":
-    if "appel" in fruit:
-       print(fruit["appel"])
-for key in fruit:
-    print(key)
-
-print("loll")
-
-
-
+def hallo():
     
-   
+
+
+
+hallo()
